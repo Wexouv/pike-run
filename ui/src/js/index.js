190936102db718339673ca64b6d0e521ac9c7d9f
ErickHack <blackdragoneraj@gmail.com>
2022-06-25 11:38:29 +0500

    Start: started with beutiful schema

---
+++ ui/src/js/index.js
@@ -0,0 +1,9 @@
+"use strict";
+const canvas = document.getElementById("pick");
+const ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
+//# sourceMappingURL=index.js.map
